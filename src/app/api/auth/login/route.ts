561e69bdffeab052749acbe546a286f3914136c9
Fadil Farhan <150595112+FadhilFarhan854@users.noreply.github.com>
2026-05-21 14:05:18 +0700

    bug-auth

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
 
     res.cookies.set(COOKIE_NAME, token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.SECURE_COOKIE === 'true',
       sameSite: 'lax',
       path: '/',
       maxAge: 60 * 60 * 24 * 7, // 7 days
